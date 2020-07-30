fdd8277512336656bbb102d3cb86c10e1968e477
himanshugharat <himanshugharat44@gmail.com>
2020-07-30 04:07:52 -0500

    ADDED:now one can play till he wins or losse his 50%  stake

--- gambler.sh
+++ gambler.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
 STAKE=100
 BET=1
-result=$((RANDOM%2))
-if [ $result -eq 1 ]
-then
-	STAKE=$((STAKE + BET))
-else
-	STAKE=$((STAKE - BET))
-fi
+dailyStake=$STAKE;
+newStake=$(((STAKE*50)/100))
+while [ $STAKE -gt $((dailyStake-newStake)) ] && [ $STAKE -lt $((dailyStake+newStake)) ]
+do
+	result=$((RANDOM%2))
 
-
+	if [ $result -eq 1 ]
+	then
+		STAKE=$((STAKE + BET))
+	else
+		STAKE=$((STAKE - BET))
+	fi
+done
+echo "you have done for the days bet"
 
