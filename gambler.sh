950ee0bce430b0192c6cf4a92b425695c8b0666f
himanshugharat <himanshugharat44@gmail.com>
2020-07-31 00:56:49 -0500

    ADDED:now we can see the luckiest and unluckiest day

--- gambler.sh
+++ gambler.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
+#constants
 STAKE=100
 BET=1
-noOfWins=0
-noOfLoss=0
+#variables
 noOfWinDay=0
 noOfLossDay=0
+#arrays
+declare -A winArray
+declare -A lossArray
+count=1
 dailyStake=$STAKE;
 percentStake=$(((STAKE*50)/100))
 function betCalculator(){
 	STAKE=100
+	noOfWins=0
+	noofLoss=0
 	dailyStake=$STAKE
 	percentStake=$(((STAKE*50)/100))
 	while [ $STAKE -gt $((dailyStake-percentStake)) ] && [ $STAKE -lt $((dailyStake+percentStake)) ]
@@ -24,10 +30,16 @@ function betCalculator(){
 			((noOfLoss++))
 		fi
 	done
-	if [ $noOfWins -gt $noOfLoss ]
+	if [ $STAKE -gt $dailyStake ]
 	then
+		winArray[$count]=$noOfWins
+		lossArray[$count]=$noOfLoss
+		((count++))
 		((noOfWinDay++))
 	else
+		winArray[$count]=$noOfWins
+		lossArray[$count]=$moOfLoss
+		((count++))
 		((noOfLossDay++))
 	fi
 	echo "you have done for the days bet"
@@ -43,8 +55,46 @@ function amountCalculator(){
 		echo "the individual lost $noOfLossDay days an amount of $result"
 	fi
 }
+
+function luckyDayFinder(){
+lucky=${winArray[1]}
+for i in ${winArray[@]}
+do
+     if [[ $i -gt $lucky ]]
+     then
+        lucky="$i"
+     fi
+done
+for i in ${winArray[@]}
+do
+        ((countj++))
+        if [[ $i -eq $lucky ]]
+        then
+
+                echo "the day lucky day was $countj with win of about $lucky times"
+        fi
+done
+lucky=${lossArray[1]}
+for i in ${lossArray[@]}
+do
+     if [[ $i -gt $lucky ]]
+     then
+        lucky="$i"
+     fi
+done
+for i in ${lossArray[@]}
+do
+	((countk++))
+	if [[ $i -eq $lucky ]]
+	then
+
+		echo "the unlucky day was $countk with loss for about $lucky times"
+	fi
+done
+}
 for (( i=0;i<30;i++ ))
 do
 betCalculator
 done
 amountCalculator
+luckyDayFinder
